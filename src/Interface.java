1fa5d3a20628468d3f421c6fa7cf272f8513ac34
Betty Anderson <101119141+bettyand@users.noreply.github.com>
2022-05-17 21:27:24 -0400

    uncomment stuff

--- src/Interface.java
+++ src/Interface.java
@@ -8,71 +8,70 @@ public class Interface {
         Scanner scanner = new Scanner(System.in);
 
         // // simple dictionary
-        // System.out.println("---Simple dictionary---");
-        // SimpleDictionary dictionary = new SimpleDictionary();
+        System.out.println("---Simple dictionary---");
+        SimpleDictionary dictionary = new SimpleDictionary();
 
-        // TextUI ui = new TextUI(scanner, dictionary);
-        // ui.start();
+        TextUI ui = new TextUI(scanner, dictionary);
+        ui.start();
 
-        // System.out.println();
-        
+        System.out.println();
 
-        // // to do list
-        // // renamed UserInterface class to TodoUI
-        // System.out.println("---To do list---");
-        // TodoList list = new TodoList();
+        // to do list
+        // renamed UserInterface class to TodoUI
+        System.out.println("---To do list---");
+        TodoList list = new TodoList();
 
-        // TodoUI todo = new TodoUI(list, scanner);
-        // todo.start();
+        TodoUI todo = new TodoUI(list, scanner);
+        todo.start();
 
-        // System.out.println();
+        System.out.println();
 
         // averages
         // renamed UserInterface to GradeUI
-        // System.out.println("---Averages---");
-        // GradeRegister register = new GradeRegister();
+        System.out.println("---Averages---");
+        GradeRegister register = new GradeRegister();
 
-        // GradeUI userInterface = new GradeUI(register, scanner);
-        // userInterface.start();
+        GradeUI userInterface = new GradeUI(register, scanner);
+        userInterface.start();
 
-        // System.out.println();
+        System.out.println();
 
         // joke manager
         // renamed UserInterface to JokeUI
-        // System.out.println("---Joke Manager---");
-        // JokeManager manager = new JokeManager();
+        System.out.println("---Joke Manager---");
+        JokeManager manager = new JokeManager();
 
-        // JokeUI ui = new JokeUI(manager, scanner);
-        // ui.start();
+        JokeUI ui2 = new JokeUI(manager, scanner);
+        ui2.start();
 
-        // System.out.println();
+        System.out.println();
 
         // TacoBoxes
         // this one was just making files buuuut here are some tests:
-        // TripleTacoBox triple = new TripleTacoBox();
-        // while (triple.tacosRemaining() > 0) {
-        //     System.out.println(triple.tacosRemaining());
-        //     triple.eat();
-        // }
-        // triple.eat();
+        TripleTacoBox triple = new TripleTacoBox();
+        while (triple.tacosRemaining() > 0) {
+            System.out.println(triple.tacosRemaining());
+            triple.eat();
+        }
+        triple.eat();
 
-        // System.out.print("How many tacos do you want? ");
-        // int tacos = Integer.valueOf(scanner.nextLine());
-        // System.out.println("Making your tacos...");
-        // if (tacos > 5) {
-        //     pause(1);
-        //     System.out.println("This is a lot of tacos!");
-        //     pause(1);
-        // } else {
-        //     pause(2);
-        // }
-        // CustomTacoBox custom = new CustomTacoBox(tacos);
-        // System.out.println("Eating all your tacos mwahaha!");
-        // while (custom.tacosRemaining() > 0) {
-        //     System.out.println(custom.tacosRemaining());
-        //     custom.eat();
-        // }
-        // custom.eat();
+        System.out.print("How many tacos do you want? ");
+        int tacos = Integer.valueOf(scanner.nextLine());
+        System.out.println("Making your tacos...");
+        if (tacos > 5) {
+            pause(1);
+            System.out.println("This is a lot of tacos!");
+            pause(1);
+        } else {
+            pause(2);
+        }
+        CustomTacoBox custom = new CustomTacoBox(tacos);
+        System.out.println("Eating all your tacos mwahaha!");
+        while (custom.tacosRemaining() > 0) {
+            System.out.println(custom.tacosRemaining());
+            custom.eat();
+        }
+        custom.eat();
     }
 
     public static void pause(int wait) {
